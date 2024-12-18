bc3e40edba02f8f842dd6124b293c21c48ec4243
simonepiase12 <149669064+simonepiase12@users.noreply.github.com>
2024-12-18 11:25:34 +0100

    template e bootsrap

--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def es_if(request):
@@ -16,3 +17,11 @@ def if_else_elif(request):
         'var3' : 100.50,
     }
     return render(request,"if_else_elif.html",context)
+
+def es_for(request):
+    context = {
+        'list1' : [1, datetime.date(2019,7,16), 'Do not give up!'],
+        'list2' : [1, datetime.date(2019,7,16), 'Do not give up!'],
+        'my_dict' : {'chiave1' : 'Valore 1', 'chiave2' : 'Valore 2'}
+    }
+    return render(request, "es_for.html", context)
