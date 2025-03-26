37d23cc5c939d9a269ef4270fea45959527ed26e
simonepiase12 <149669064+simonepiase12@users.noreply.github.com>
2025-03-26 11:32:48 +0100

    Forms

--- forms_app/urls.py
+++ forms_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import contatti,lista
+from .views import contatti,lista,modifica_contatto,elimina_contatto
 
 app_name = "forms_app"
 
 urlpatterns = [
     path('contattaci/', contatti, name='contatti'),
     path('lista', lista, name='lista'),
+    path('elimina_contatto/<int:pk>', elimina_contatto, name='elimina_contatto'),
+    path('modifica_contatto/<int:pk>', modifica_contatto, name='modifica_contatto'),
+
 ]
